fac124357fe047fa60e18006ac5a149abd6c3faa
Marion Penel <marionpenel@hotmail.fr>
2026-03-30 17:06:45 +0200

    middleware CORS

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ mongoose.connect('mongodb+srv://marionpenel3_db_user:marionpenel3_db_user@cluste
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch((error) => console.error('Erreur de connexion à MongoDB :', error));
 
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use((req, res, next) => {
   res.json({ message: 'Votre serveur Express fonctionne !' });
 });
